853b96b439a5667eb2f716398bc664ffdd09440f
Manjusha Kumari <manjushatyagi17@gmail.com>
2024-05-27 00:31:01 +0530

    basic model created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import Button from './components/button';
+
+import './style.css';
 
 function App() {
-  return <p>Todo App</p>
+  return (
+    <div className='todo-container'>
+      <Header />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <Button />
+    </div>
+  )
 };
 
 export default App;
